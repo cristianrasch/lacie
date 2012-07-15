70187155cb37c30a31cecd2d1f9b12b10361308a
Cristian Rasch <cristianrasch@gmail.com>
2012-07-15 18:28:26 -0300

    Migración del sitio del LACIE en Drupal

--- lacie.rb
+++ lacie.rb
@@ -22,6 +22,66 @@ class Lacie < Sinatra::Base
     erb :index
   end
   
+  get "/actividades/2012" do
+    erb :"activities/2012"
+  end
+  
+  get "/actividades/2011" do
+    erb :"activities/2011"
+  end
+  
+  get "/actividades/2010" do
+    erb :"activities/2010"
+  end
+  
+  get "/actividades/2009" do
+    erb :"activities/2009"
+  end
+  
+  get "/actividades/2008" do
+    erb :"activities/2008"
+  end
+  
+  get "/actividades/2007" do
+    erb :"activities/2007"
+  end
+  
+  get "/actividades/2005-2006" do
+    erb :"activities/2005-2006"
+  end
+  
+  get "/actividades" do
+    erb :"activities/index"
+  end
+  
+  get "/integrantes" do
+    erb :staff
+  end
+
+  get "/instrumental/kits-de-desarrollo" do
+    erb :"tools/kits"
+  end
+  
+  get "/instrumental/maquinas-y-herramientas" do
+    erb :"tools/machines"
+  end
+  
+  get "/instrumental" do
+    erb :"tools/index"
+  end
+  
+  get "/publicaciones" do
+    erb :publications
+  end
+  
+  get "/reglamento" do
+    erb :bylaws
+  end
+  
+  get "/contacto" do
+    erb :contact_us
+  end
+  
   not_found do
     flash[:error] = "No se encontró la página"
     redirect to("/")
--- test/lacie_test.rb
+++ test/lacie_test.rb
@@ -8,6 +8,8 @@ class LacieTest < CapybaraTestCase
     
     assert_equal "/", current_path
     assert page.has_content?("Laboratorio Abierto de la Carrera de Ingeniería Electrónica")
+    assert page.has_css?('ul.nav-list li.active')
+    assert_equal "Home", page.find('ul.nav-list li.active').text
   end
   
   def test_visiting_an_invalid_page
@@ -15,5 +17,155 @@ class LacieTest < CapybaraTestCase
     
     assert_equal "/", current_path
     assert page.has_content?("Laboratorio Abierto de la Carrera de Ingeniería Electrónica")
+  end
+  
+  def test_activities
+    visit "/actividades"
+    
+    assert_equal "/actividades", current_path
+    assert page.has_content?("Desde el año 2005 cuando fue creado, el LACIE colabora y organiza actividades y eventos")
+    assert_equal "Actividades", page.find('ul.nav-list li.active').text
+    refute page.has_css?('ul.nav-tabs li.active')
+  end
+  
+  def test_activities_2012
+    visit "/actividades/2012"
+    
+    assert_equal "/actividades/2012", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Actividades 2012")
+    assert_equal "Actividades", page.find('ul.nav-list li.active').text
+    assert page.has_css?('ul.nav-tabs li.active')
+    assert_match /2012/, page.find('ul.nav-tabs li.active').text
+  end
+  
+  def test_activities_2011
+    visit "/actividades/2011"
+    
+    assert_equal "/actividades/2011", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Actividades 2011")
+    assert_equal "Actividades", page.find('ul.nav-list li.active').text
+    assert_match /2011/, page.find('ul.nav-tabs li.active').text
+  end
+  
+  def test_activities_2010
+    visit "/actividades/2010"
+    
+    assert_equal "/actividades/2010", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Actividades 2010")
+    assert_equal "Actividades", page.find('ul.nav-list li.active').text
+    assert_match /2010/, page.find('ul.nav-tabs li.active').text
+  end
+  
+  def test_activities_2009
+    visit "/actividades/2009"
+    
+    assert_equal "/actividades/2009", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Actividades 2009")
+    assert_equal "Actividades", page.find('ul.nav-list li.active').text
+    assert_match /2009/, page.find('ul.nav-tabs li.active').text
+  end
+  
+  def test_activities_2008
+    visit "/actividades/2008"
+    
+    assert_equal "/actividades/2008", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Actividades 2008")
+    assert_equal "Actividades", page.find('ul.nav-list li.active').text
+    assert_match /2008/, page.find('ul.nav-tabs li.active').text
+  end
+  
+  def test_activities_2007
+    visit "/actividades/2007"
+    
+    assert_equal "/actividades/2007", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Actividades 2007")
+    assert_equal "Actividades", page.find('ul.nav-list li.active').text
+    assert_match /2007/, page.find('ul.nav-tabs li.active').text
+  end
+  
+  def test_activities_2005_2006
+    visit "/actividades/2005-2006"
+    
+    assert_equal "/actividades/2005-2006", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Actividades 2005 y 2006")
+    assert_equal "Actividades", page.find('ul.nav-list li.active').text
+    assert_match /2005-2006/, page.find('ul.nav-tabs li.active').text
+  end
+  
+  def test_staff
+    visit "/integrantes"
+    
+    assert_equal "/integrantes", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Integrantes del LACIE")
+    assert_equal "Integrantes", page.find('ul.nav-list li.active').text
+  end
+  
+  def test_tools
+    visit "/instrumental"
+    
+    assert_equal "/instrumental", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Instrumental")
+    assert_equal "Instrumental", page.find('ul.nav-list li.active').text
+    refute page.has_css?('ul.nav-tabs li.active')
+  end
+  
+  def test_kits
+    visit "/instrumental/kits-de-desarrollo"
+    
+    assert_equal "/instrumental/kits-de-desarrollo", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Kits de desarrollo")
+    assert_equal "Instrumental", page.find('ul.nav-list li.active').text
+    assert page.has_css?('ul.nav-tabs li.active')
+    assert_match /kits/i, page.find('ul.nav-tabs li.active').text
+  end
+  
+  def test_machines
+    visit "/instrumental/maquinas-y-herramientas"
+    
+    assert_equal "/instrumental/maquinas-y-herramientas", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Máquinas y herramientas")
+    assert_equal "Instrumental", page.find('ul.nav-list li.active').text
+    assert page.has_css?('ul.nav-tabs li.active')
+    assert_match /máquinas/i, page.find('ul.nav-tabs li.active').text
+  end
+  
+  def test_publications
+    visit "/publicaciones"
+    
+    assert_equal "/publicaciones", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Publicaciones")
+    assert_equal "Publicaciones", page.find('ul.nav-list li.active').text
+    assert page.has_css?('ul.nav-tabs li.active')
+    assert_match /2011/, page.find('ul.nav-tabs li.active').text
+  end
+  
+  def test_publications
+    visit "/reglamento"
+    
+    assert_equal "/reglamento", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Reglamento")
+    assert_equal "Reglamento", page.find('ul.nav-list li.active').text
+  end
+  
+  def test_contact_us
+    visit "/contacto"
+    
+    assert_equal "/contacto", current_path
+    assert page.has_selector?("h1")
+    assert page.has_content?("Contacto")
+    assert_equal "Contacto", page.find('ul.nav-list li.active').text
   end
 end
